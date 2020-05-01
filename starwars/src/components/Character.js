e02adb65254385ade8adcc4257055efa5683b9af
Claudia Sol <Claudia@Claudias-MacBook-Pro.local>
2020-05-01 19:46:23 -0400

    added pikachu api

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,22 @@
 // Write your Character component here
+import React, {useState} from "react";
+
+import axios from "axios";
+
+const Character = () => {
+    axios.get("https://pokeapi.co/api/v2/pokemon/25/")
+    .then(response => {
+        console.log(response);
+    })
+    .catch(err => {
+        console.log(err);
+    })
+
+return (
+    <div>
+        Test
+    </div>
+)
+}
+
+export default Character;
